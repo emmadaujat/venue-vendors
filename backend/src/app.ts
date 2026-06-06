a52b7f1a8212bb7dea35db18a30cdb715a1314d2
emmadaujat <s4151401@student.rmit.edu.au>
2026-06-06 12:13:06 +1000

    trying to fix compliance doc error

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,8 +15,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.use("/api", authRoutes);
 
